a88af059efc585e495d58473b515223d9ca0c3fc
Aleksandrus <arielaleksandrus@hotmail.com>
2016-06-08 14:05:54 -0300

    Removes possibility of users to sign in. Only clients should be able to sign in

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,19 +2,22 @@ class Api::V1::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    flash[:info] = 'Not available'
+    redirect_to root_path
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    flash[:info] = 'Not available'
+    redirect_to root_path
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    flash[:info] = 'Not available'
+    redirect_to root_path
+  end
 
   # protected
 
